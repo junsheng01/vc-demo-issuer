87a905f7837496ab30d39d9f62da788e1671bc8c
yankai14 <limyk2014@gmail.com>
2021-05-17 23:57:58 +0800

    Added tests and added validation for empty fields in Application.tsx

--- src/components/admin/AdminList.test.tsx
+++ src/components/admin/AdminList.test.tsx
@@ -2,20 +2,41 @@ import React from "react";
 import AdminList from "components/admin/AdminList";
 import { act, render } from "@testing-library/react";
 import {createMemoryHistory} from 'history';
-import {Router} from 'react-router-dom';
+import { Router, MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event'
 
 const history = createMemoryHistory()
+
 const mockData = {
-    applicationID: '000000'
+  approved: false,
+  username: "tester",
+  applicationID: "0000000",
+  payload: {
+      givenName: "trest",
+      holderDid: "did:elem:EiCUQ8lQ4FnbNO5jFQaEMGnZ8uPO46VEUTicvd3ky8YZnQ;elem:initial-state=eyJwcm90ZWN0ZWQiOiJleUp2Y0dWeVlYUnBiMjRpT2lKamNtVmhkR1VpTENKcmFXUWlPaUlqY0hKcGJXRnllU0lzSW1Gc1p5STZJa1ZUTWpVMlN5SjkiLCJwYXlsb2FkIjoiZXlKQVkyOXVkR1Y0ZENJNkltaDBkSEJ6T2k4dmR6TnBaQzV2Y21jdmMyVmpkWEpwZEhrdmRqSWlMQ0p3ZFdKc2FXTkxaWGtpT2x0N0ltbGtJam9pSTNCeWFXMWhjbmtpTENKMWMyRm5aU0k2SW5OcFoyNXBibWNpTENKMGVYQmxJam9pVTJWamNESTFObXN4Vm1WeWFXWnBZMkYwYVc5dVMyVjVNakF4T0NJc0luQjFZbXhwWTB0bGVVaGxlQ0k2SWpBelltRTRPV1EzTURZeVlqaGpNREF4WWpZd1lUSXlNakE0TVdJME5ERmpNV1EzTURBek9UTTVPREk1Wldaa05tTTFabU14WTJJNU4yVTBORFkzWXpJMk1TSjlMSHNpYVdRaU9pSWpjbVZqYjNabGNua2lMQ0oxYzJGblpTSTZJbkpsWTI5MlpYSjVJaXdpZEhsd1pTSTZJbE5sWTNBeU5UWnJNVlpsY21sbWFXTmhkR2x2Ymt0bGVUSXdNVGdpTENKd2RXSnNhV05MWlhsSVpYZ2lPaUl3TWpBellqWXlOR00xWmpFek56QXpORFJtWWpnMVpqZGhPV1EyWVRZMlpUVmlaalpqTlRSaU1XSXlNRFEyTURJeFptVXpZakl3WlRNNU56WTNZalpqWmpZaWZWMHNJbUYxZEdobGJuUnBZMkYwYVc5dUlqcGJJaU53Y21sdFlYSjVJbDBzSW1GemMyVnlkR2x2YmsxbGRHaHZaQ0k2V3lJamNISnBiV0Z5ZVNKZGZRIiwic2lnbmF0dXJlIjoiRDVCZ1BYaGlreDU2dl9YNDBkVjRtZ2RQbnUtblBBNmc5c0gwemNEclpIWW42cEQyZHlocmpLTU9lT01UTXoxOFROcE0xZW1kMmZCaFdiLXZ6SmtsLVEifQ",
+      familyName: "yete",
+      idClass: "{\"drivingLicenseID\":\"whuifhwf\",\"country\":\"Singapore\",\"drivingClass\":\"3\",\"email\":\"test@gmail.com\",\"issuerOrganization\":\"Automobile Association of Singapore\",\"affinidiDrivingLicenseID\":\"VNFkSM8rF2\"}",
+      issueDate: "10/10/2018"
+  },
+  docID: "UvF3osjYVncXjrxElpnI"
 }
 
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
 describe("AdminList Component Test", () => {
+
   test("Renders the component", () => {
     const { getByText, getByRole } = render(    
-        <Router history={history}>
-            <AdminList data={mockData}/>
-        </Router>
+      <MemoryRouter>
+        <AdminList data={mockData}/>
+      </MemoryRouter>
     )
 
     const applicationNumber = getByText(/Application ID:/i)
@@ -24,4 +45,18 @@ describe("AdminList Component Test", () => {
     expect(applicationNumber).toBeInTheDocument();
     expect(viewMoreButton).toBeInTheDocument();
   });
+
+  test("Button calls history.push", () => {
+    const { getByRole } = render(
+      <Router history={history}>
+        <AdminList data={mockData}/>
+      </Router>
+    );
+
+    const viewMoreButton = getByRole('button', {name: 'View more'});
+
+    userEvent.click(viewMoreButton);
+    expect(mockHistoryPush).toHaveBeenCalledTimes(1)
+  })
+
 });
--- src/pages/application/Application.tsx
+++ src/pages/application/Application.tsx
@@ -1,38 +1,38 @@
 import React, { useState, useContext } from 'react';
 import AppContext from 'context/app';
-import {Button, FormControl, FormGroup, FormLabel, FormFile} from 'react-bootstrap';
+import {Button, Form} from 'react-bootstrap';
 import ApiService from 'utils/apiService';
 import 'pages/application/Application.scss'
 import firebase from 'utils/firebase/firebase';
 import randomstring from 'randomstring';
 
 interface IBaseVCData {
-    givenName: string;
-    familyName: string;
-    issueDate: string;
-  }
-  
-  interface IExtendVCData {
-    drivingLicenseID: string;
-    country: string;
-    drivingClass: string;
-    email: string;
-    issuerOrganization: string;
-  }
-  
-  const defaultBaseVCData: IBaseVCData = {
-    givenName: '',
-    familyName: '',
-    issueDate: ''
-  }
-  
-  const defaultExtendVCData: IExtendVCData = {
-    drivingLicenseID: '',
-    country: 'Singapore',
-    drivingClass: '1',
-    email: '',
-    issuerOrganization: 'Automobile Association of Singapore'
-  }
+  givenName: string;
+  familyName: string;
+  issueDate: string;
+}
+
+interface IExtendVCData {
+  drivingLicenseID: string;
+  country: string;
+  drivingClass: string;
+  email: string;
+  issuerOrganization: string;
+}
+
+const defaultBaseVCData: IBaseVCData = {
+  givenName: '',
+  familyName: '',
+  issueDate: ''
+}
+
+const defaultExtendVCData: IExtendVCData = {
+  drivingLicenseID: '',
+  country: 'Singapore',
+  drivingClass: '1',
+  email: '',
+  issuerOrganization: 'Automobile Association of Singapore'
+}
 
 interface IPayload extends IBaseVCData{
   idClass: string;
@@ -41,40 +41,57 @@ interface IPayload extends IBaseVCData{
 
 const Application: React.FC = (): React.ReactElement => {
     const {appState} = useContext(AppContext);
-    const [inputDID, setinputDID] = useState(appState.didToken || '')
+    const [inputDID, setinputDID] = useState(appState.didToken || '');
 
-    const [baseVCData, setBaseVCData] = useState<IBaseVCData>(defaultBaseVCData)
+    const [baseVCData, setBaseVCData] = useState<IBaseVCData>(defaultBaseVCData);
   
-    const [extendVCData, setExtendVCData] = useState<IExtendVCData>(defaultExtendVCData)
+    const [extendVCData, setExtendVCData] = useState<IExtendVCData>(defaultExtendVCData);
+    const [validated, setValidated] = useState(false);
 
     /**
      * Function for issuing an unsigned employment VC.
      * */
+
     const issueDrivingLicensePersonVC = async () => {
-        try {
-          const { givenName, familyName, issueDate } = baseVCData;
+      try {
+    
+        setValidated(true);
 
-          // Generate a random Affinidi Driving License ID, which will double up as an application ID
-          const applicationID: string = randomstring.generate(10);
-          const vcToStringify = {...extendVCData, affinidiDrivingLicenseID: applicationID}
-          
-          const payload: IPayload = {
-            givenName,
-            familyName,
-            issueDate,
-            idClass: JSON.stringify(vcToStringify),
-            holderDid: inputDID || appState.didToken || '',
-          }
+        const { givenName, familyName, issueDate } = baseVCData;
 
-          // Store unsignedVC into issuer's datsabase
-          const db = firebase.firestore();
-          db.collection('drivinglicense-waiting-approval').add({username: appState.username, payload, applicationID, approved: false})
-
-          alert('You have successfully submitted your application.');
-        } catch (error) {
-            ApiService.alertWithBrowserConsole(error.message);
+        // Generate a random Affinidi Driving License ID, which will double up as an application ID
+        const applicationID: string = randomstring.generate(10);
+        const vcToStringify = {...extendVCData, affinidiDrivingLicenseID: applicationID}
+        
+        const payload: IPayload = {
+          givenName,
+          familyName,
+          issueDate,
+          idClass: JSON.stringify(vcToStringify),
+          holderDid: inputDID || appState.didToken || '',
         }
+
+        // Store unsignedVC into issuer's datsabase
+        const db = firebase.firestore();
+        db.collection('drivinglicense-waiting-approval').add({username: appState.username, payload, applicationID, approved: false})
+
+        alert('You have successfully submitted your application.');
+      } catch (error) {
+          ApiService.alertWithBrowserConsole(error.message);
+      }
     }
+
+    const handleSubmit = (event: any) => {
+      const form = event.currentTarget;
+      if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+      } else {
+        issueDrivingLicensePersonVC()
+      }
+  
+      setValidated(true);
+    };
     
     const resetToDefaults = () => {
       setinputDID(appState.didToken || '')
@@ -101,60 +118,68 @@ const Application: React.FC = (): React.ReactElement => {
           </Button>
 
           <p><strong>Step 1:</strong>Please fill in details of your driving license</p>
-          <FormGroup controlId='email'>
-            <FormLabel className='label' style={{margin: '10px 0 0 0'}}>Email Address:</FormLabel>
-            <FormControl name='email' type='text' value={extendVCData.email} onChange={e => updateExtendBaseVC(e)}/>
-          </FormGroup>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group controlId='email'>
+              <Form.Label className='label' style={{margin: '10px 0 0 0'}}>Email Address:</Form.Label>
+              <Form.Control required name='email' type='text' value={extendVCData.email} onChange={e => updateExtendBaseVC(e)}/>
+              <Form.Control.Feedback type="invalid">Please provide a valid Email Address.</Form.Control.Feedback>
+            </Form.Group>
 
-          <FormGroup controlId='givenName'>
-            <FormLabel className='label' style={{margin: '10px 0 0 0'}}>Given Name:</FormLabel>
-            <FormControl name='givenName' type='text' value={baseVCData.givenName} onChange={e => updateBaseVC(e)}/>
-          </FormGroup>
+            <Form.Group controlId='givenName'>
+              <Form.Label className='label' style={{margin: '10px 0 0 0'}}>Given Name:</Form.Label>
+              <Form.Control required name='givenName' type='text' value={baseVCData.givenName} onChange={e => updateBaseVC(e)}/>
+              <Form.Control.Feedback type="invalid"> Please provide a Given Name. </Form.Control.Feedback>
+            </Form.Group>
 
-          <FormGroup controlId='familyName'>
-            <FormLabel style={{margin: '10px 0 0 0'}}>Family Name:</FormLabel>
-            <FormControl name='familyName' type='text' value={baseVCData.familyName} onChange={e => updateBaseVC(e)}/>
-          </FormGroup>
+            <Form.Group controlId='familyName'>
+              <Form.Label style={{margin: '10px 0 0 0'}}>Family Name:</Form.Label>
+              <Form.Control required name='familyName' type='text' value={baseVCData.familyName} onChange={e => updateBaseVC(e)}/>
+              <Form.Control.Feedback type="invalid"> Please provide a Family Name. </Form.Control.Feedback>
+            </Form.Group>
 
-          <FormGroup controlId='issueDate'>
-            <FormLabel style={{margin: '10px 0 0 0'}}>Date of Issuance:</FormLabel>
-            <FormControl name='issueDate' type='text' value={baseVCData.issueDate} onChange={e => updateBaseVC(e)}/>
-          </FormGroup>
+            <Form.Group controlId='issueDate'>
+              <Form.Label style={{margin: '10px 0 0 0'}}>Date of Issuance:</Form.Label>
+              <Form.Control required name='issueDate' type='text' value={baseVCData.issueDate} onChange={e => updateBaseVC(e)}/>
+              <Form.Control.Feedback type="invalid"> Please provide a Date. </Form.Control.Feedback>
+            </Form.Group>
 
-          <FormGroup controlId='drivingLicense'>
-            <FormLabel style={{margin: '10px 0 0 0'}}>Driving License ID:</FormLabel>
-            <FormControl name='drivingLicenseID' type='text' value={extendVCData.drivingLicenseID} onChange={e => updateExtendBaseVC(e)}/>
-          </FormGroup>
+            <Form.Group controlId='drivingLicense'>
+              <Form.Label style={{margin: '10px 0 0 0'}}>Driving License ID:</Form.Label>
+              <Form.Control required name='drivingLicenseID' type='text' value={extendVCData.drivingLicenseID} onChange={e => updateExtendBaseVC(e)}/>
+              <Form.Control.Feedback type="invalid"> Please provide a valid Driving License ID. </Form.Control.Feedback>
+            </Form.Group>
 
-          <FormGroup controlId='drivingClass'>
-            <FormLabel style={{margin: '10px 0 0 0'}}>Driving Class:</FormLabel>
-            <FormControl name='drivingClass' as="select" value={extendVCData.drivingClass} onChange={e => updateExtendBaseVC(e)}>
-              <option>1</option>
-              <option>2</option>
-              <option>2A</option>
-              <option>2B</option>
-              <option>3</option>
-              <option>3A</option>
-              <option>3C</option>
-              <option>3CA</option>
-              <option>4</option>
-              <option>4A</option>
-              <option>5</option>
-            </FormControl>
-          </FormGroup>
+            <Form.Group controlId='drivingClass'>
+              <Form.Label style={{margin: '10px 0 0 0'}}>Driving Class:</Form.Label>
+              <Form.Control name='drivingClass' as="select" value={extendVCData.drivingClass} onChange={e => updateExtendBaseVC(e)}>
+                <option>1</option>
+                <option>2</option>
+                <option>2A</option>
+                <option>2B</option>
+                <option>3</option>
+                <option>3A</option>
+                <option>3C</option>
+                <option>3CA</option>
+                <option>4</option>
+                <option>4A</option>
+                <option>5</option>
+              </Form.Control>
+            </Form.Group>
 
-          <div style={{margin: '30px 0'}}>
-            <p><strong>Step 2:</strong>Upload Proof of Driving License</p>
-            <FormFile id="formcheck-api-regular">
-              <FormFile.Label>Proof of Driving License</FormFile.Label>
-              <FormFile.Input />
-            </FormFile>
-          </div>
-          
-          <Button 
-            onClick={e => issueDrivingLicensePersonVC()}
-            >Submit
-          </Button>
+            <div style={{margin: '30px 0'}}>
+              <p><strong>Step 2:</strong>Upload Proof of Driving License</p>
+              <Form.File id="formcheck-api-regular">
+                <Form.File.Label>Proof of Driving License</Form.File.Label>
+                <Form.File.Input />
+                <Form.Control.Feedback type="invalid"> Please provide proof of your Driving License. </Form.Control.Feedback>
+              </Form.File>
+            </div>
+            
+            <Button 
+              type="submit"
+              >Submit
+            </Button>
+          </Form>
         </div>
       </div>
     )
